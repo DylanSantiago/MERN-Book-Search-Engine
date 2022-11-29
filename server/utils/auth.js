8d800373e79ccb9ee6d5704b33509a47a0b52f6b
DylanSantiago <2dylansantiago@gmail.com>
2022-11-29 18:06:46 -0500

    made middlewarecompatible with graphql

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -25,11 +25,9 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
+    return req;
 
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };

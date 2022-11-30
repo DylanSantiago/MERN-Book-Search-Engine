0667cf452106f81640a1dcb402809be7befefee8
DylanSantiago <2dylansantiago@gmail.com>
2022-11-30 17:35:20 -0500

    removed api folder in schema directory and exported typedefs and resolvers in index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,12 +1,4 @@
-const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
-router.use('/api', apiRoutes);
-
-// serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-});
-
-module.exports = router;
+module.exports = { typeDefs, resolvers };
